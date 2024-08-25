bd643eff274e49d72c16d8d5c4fb7ff15377e944
shali <shalini123suman@gmail.com>
2024-08-25 23:31:49 +0530

    refactoring of unused code

--- app/src/main/java/com/example/test/data/model/Movies.kt
+++ app/src/main/java/com/example/test/data/model/Movies.kt
@@ -4,26 +4,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-//"backdrop_path": "/sRLC052ieEzkQs9dEtPMfFxYkej.jpg",
-//            "id": 848326,
-//            "title": "Rebel Moon - Part One: A Child of Fire",
-//            "original_title": "Rebel Moon - Part One: A Child of Fire",
-//            "overview": "When the ruthless forces of the Motherworld threaten a quiet farming village on a distant moon, a mysterious outsider becomes its best hope for survival.",
-//            "poster_path": "/ui4DrH1cKk2vkHshcUcGt2lKxCm.jpg",
-//            "media_type": "movie",
-//            "adult": false,
-//            "original_language": "en",
-//            "genre_ids": [
-//                878,
-//                18,
-//                28
-//            ],
-//            "popularity": 94.253,
-//            "release_date": "2023-12-15",
-//            "video": false,
-//            "vote_average": 6.3,
-//            "vote_count": 2016
-
 @Entity
 data class Movie(
     @PrimaryKey
@@ -46,7 +26,5 @@ data class Movie(
     @SerializedName("vote_average")
     val voteAverage: String,
     @SerializedName("vote_count")
-    val voteCount: String,
-//    @SerializedName("genre_ids")
-//    val genreIds: List<Int>,
+    val voteCount: String
 )
